1d8cb32a0c4efb24e868dd1562d9e73abc83d655
Loichuet1 <43272013+Loichuet1@users.noreply.github.com>
2024-04-01 19:03:35 +0200

    Init Vite and React

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='Grid'>
+        TEST
+        <p>TESt</p>
+        <p>TESt</p>
+      </div>
+    </>
+  )
+}
+
+export default App
