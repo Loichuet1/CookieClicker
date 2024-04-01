c6703425bb1457eace7aada17ddbc2a7220b1bce
Loichuet1 <43272013+Loichuet1@users.noreply.github.com>
2024-04-01 20:26:31 +0200

    Base layout design

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,36 @@
 import { useState } from 'react'
 import './App.css'
 
+
 function App() {
+
+  const [count, setCount] = useState(0);
+
   return (
     <>
-      <div className='Grid'>
-        TEST
-        <p>TESt</p>
-        <p>TESt</p>
+      <div>
+         <nav className='Nav'>Nav {count}</nav>
+         <div className='mainSection'>
+          <img className='playerImg' alt='playerImage' onClick={() => setCount(count+1)} src='https://img.freepik.com/premium-vector/cartoon-business-team-working-startup-project_81522-4573.jpg'></img>  
+         </div>
+
+         <div className='flexBox' >
+           <div className='bonusSection'>    
+              <div className='bonusItem'>Test</div>
+              <div className='bonusItem'>Test</div>
+              <div className='bonusItem'>Test</div>
+              <div className='bonusItem'>Test</div>
+           </div>
+           <div className='shopSection'>
+    
+              <div className='shopItem'>Test</div>
+              <div className='shopItem'>Test</div>
+              <div className='shopItem'>Test</div>
+              <div className='shopItem'>Test</div>
+              <div className='shopItem'>Test</div>   
+              
+           </div>
+         </div>
       </div>
     </>
   )
