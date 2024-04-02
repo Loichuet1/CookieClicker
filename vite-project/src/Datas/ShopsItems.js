a4538acb6f4e52bae28e9ebebf576ed9d4a63c89
Abys-Loic <93723439+Abys-Loic@users.noreply.github.com>
2024-04-02 17:16:13 +0200

    Adding shopItem and score management

---
+++ vite-project/src/Datas/ShopsItems.js
@@ -0,0 +1,35 @@
+const shopItems = 
+[
+    {
+     id : 1,
+     name : "Margarita",
+     value : 8 ,
+     boostValue : 2 
+    },
+    {
+        id : 3,
+        name : "Piña Colada",
+        value : 500,
+        boostValue : 10
+    },
+    {
+        id : 2,
+        name : "Mojito" ,
+        value : 7500,
+        boostValue : 120
+    },   
+    {
+        id : 4,
+        name : "Cosmos" ,
+        value : 93000,
+        boostValue : 950
+    },    
+    {
+        id : 5,
+        name : "Old Fashioned" ,
+        value : 15669996,
+        boostValue : 2500
+    }
+]
+
+export default shopItems
